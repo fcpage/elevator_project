96d15415fbcdaff9ea8b79a186f0359d960f533d
Nigel Sinclair <sincngraeme@gmail.com>
2026-05-28 20:21:31 -0400

    added stdio logging and CAN comms

---
+++ FloorController/Core/Inc/basic_defs.h
@@ -0,0 +1,23 @@
+/**
+******************************************************************************** 
+* @file     : basic_defs.h
+* @brief    : Provides basic QOL type defintion shorthands
+* By        : Nigel Sinclair
+******************************************************************************** 
+*/
+
+#ifndef __BASIC_DEFS_H
+#define __BASIC_DEFS_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef int8_t  i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
+typedef uint8_t  u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+#endif
