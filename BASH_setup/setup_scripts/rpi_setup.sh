424f805a262dd298aaef5586de4c174a2386ebef
fcpage <colmanfp@gmail.com>
2026-06-10 17:53:05 -0400

    db

--- BASH_setup/setup_scripts/rpi_setup.sh
+++ BASH_setup/setup_scripts/rpi_setup.sh
@@ -49,20 +49,6 @@ wait_fn	#waiting til the pen stops scratching
 xdg-open http://localhost/phpmyadmin	#open info in the browser to test activity
 echo "password: ese"	#don't share this with anyone
 wait_fn	#secret silence
-sudo mysql -u root -p -P 3306 #give access to all databases
-wait_fn #let that sink in
-echo "password: ese"	#you probably should change at least one of your passwords...
-wait_fn #HOLD IT RIGHT THERE!
-sudo mysql -u phpmyadmin -p -P 3306 #giving access to php
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('ese')" | mysql -u root #setting password for the root in mysql
-mysql -u root -p << POM #creating ese user so we aren't in root all the time
-USE mysql;
-CREATE USER 'ese'@'%' IDENTIFIED BY 'ese';
-GRANT ALL PRIVILEGES ON *.* TO 'ese'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-quit;
-POM #multi-line input sequence... the commands are pretty self-evident
-sudo service mysql restart #restart mysql so that the changes can take effect
 python --version	#list python version
 gcc -v	#list gcc version
 g++ -v	#list g++ version
