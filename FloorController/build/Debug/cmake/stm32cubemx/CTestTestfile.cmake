186d2851974d8294120c1256314c76d7c8d917c6
Nigel Sinclair <sincngraeme@gmail.com>
2026-06-17 16:30:12 -0400

    removed build files from git

--- FloorController/build/Debug/cmake/stm32cubemx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nigel/Projects/school/ElevatorProject/FloorController/cmake/stm32cubemx
-# Build directory: /home/nigel/Projects/school/ElevatorProject/FloorController/build/Debug/cmake/stm32cubemx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
