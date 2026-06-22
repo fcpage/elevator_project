9b8a8d5b8fba0357ecae8aebcedc53caa50a60d0
sincngraeme <166749247+sincngraeme@users.noreply.github.com>
2026-06-22 16:56:24 -0400

    Revert "Arrival notifications"

--- FloorController/Core/Src/usermain.c
+++ FloorController/Core/Src/usermain.c
@@ -8,7 +8,6 @@
 
 // PSA: ONLY EVER INCLUDE THIS YOU WILL REGRET INCLUDING OTHER HAL HEADERS
 #include "stm32f3xx_hal.h" 
-#include "stm32f3xx_hal_gpio.h"
 
 enum FloorButtons {
     NO_BUTTON_PRESSED	= 0,
@@ -41,13 +40,12 @@ static u8   BUTTON = NO_BUTTON_PRESSED;		// Initial value is that no BUTTON has 
 #endif
 
 typedef struct { 
-    GPIO_TypeDef*       led_port;   // Will not change but function calls discard const
-    const u16           led_pin;
-    const u8            msg;
-    bool                pending_request;
+    GPIO_TypeDef* led_port; 
+    u16           led_pin;
+    u8            msg;
 } FloorData;
 
-static FloorData event_lookup[] = {
+static const FloorData event_lookup[] = {
     { /* NO_BUTTON_PRESSED   */ },
     /* FL1_BUTTON_PRESSED  */ 
     { 
@@ -76,7 +74,7 @@ static FloorData event_lookup[] = {
 };
 
 
-static FloorData* floor = &event_lookup[NO_BUTTON_PRESSED];
+static const FloorData* floor = &event_lookup[NO_BUTTON_PRESSED];
 
 void user_main(void) {
 
@@ -90,10 +88,9 @@ void user_main(void) {
             HAL_Delay(100);
             HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_5);
             HAL_Delay(100);
-            dbglog(LVL3, "Hearbeat request recieved: %d\n", RxData[0]);
+            dbglog(LVL3, "Hearbeat request recieved: %s\n", RxData);
             TxData[0] = HB_OK;
-            // HAL_Delay(100 * (NODE_ID & 0b11));
-            dbglog(LVL3, "Sending heartbeat response: %d\n", TxData[0]);
+            dbglog(LVL3, "Sending heartbeat response: %s\n", TxData);
             if(HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox) != HAL_OK) {
                 panic("Failed to send CAN message");
             }
@@ -103,20 +100,17 @@ void user_main(void) {
         case SC_POS_FLOOR_2: [[ fallthrough ]];
         case SC_POS_FLOOR_3: 
         {
-            dbglog(LVL1, "Received floor status message: %d\n", RxData[0]);
+            dbglog(LVL1, "Received floor status message: %s\n", RxData);
             /* Floor number is indicated by the lower two bits in the node ID 
              * and the MSG (checks if the elevator is at our floor) */
             if( (RxData[0] & 0b11) == (NODE_ID & 0b11) ) {
-                if(floor->pending_request) {
-                    HAL_GPIO_TogglePin(floor->led_port, floor->led_pin);
-                    floor->pending_request = false;
-                }
+                // TODO: turn off button led (unknown at this time)
             }
             goto reset_Rx_buffer;
         }
 #endif
         default:
-            dbglog(LVL1, "Ignoring CAN msg: %d\n", RxData[0]);
+            dbglog(LVL1, "Unknown CAN msg: %s\n", RxData);
         reset_Rx_buffer:
             memset(RxData, 0, sizeof(RxData));  // Reset the buffer
             break;
@@ -129,25 +123,17 @@ void user_main(void) {
             panic("Invalid button");
         } else {
             floor = &event_lookup[BUTTON];
-#ifndef CAN_COMMON
-            if (floor->pending_request) {
-                dbglog(LVL2, "Request for current floor pending, ignoring request");
-                BUTTON = NO_BUTTON_PRESSED;
-                return;
-            }
-#endif
-            // Turn on button LED
-            HAL_GPIO_TogglePin(floor->led_port, floor->led_pin);     // Discards const, does not matter here
-            floor->pending_request = true;
+            // Turn on shield LED
+            HAL_GPIO_TogglePin(floor->led_port, floor->led_pin);
+            HAL_Delay(2000);
+            // TODO: turn on button led (unknown at this time)
             TxData[0] = floor->msg;     // Store the appropriate message for the given button
             if (HAL_CAN_AddTxMessage(&hcan, &TxHeader, TxData, &TxMailbox) != HAL_OK) {
                 panic("Failed to send CAN message");
             }
             dbglog(LVL1, "CAN message sent: %d\n", TxData[0]);
-#ifdef CAN_COMMON
-            HAL_Delay(2000);
-            HAL_GPIO_TogglePin(floor->led_port, floor->led_pin);
-#endif
+            // Turn off shield LED
+            HAL_GPIO_TogglePin(floor->led_port, floor->led_pin);  	
         }
         BUTTON = NO_BUTTON_PRESSED; 								// Reset the BUTTON flag
     }
@@ -175,7 +161,7 @@ void user_CAN_init(void) {
         /* Set FilterIdHigh bits by choosing an ID and aligning the bits in the filter register 
          * with the receive register by shifting << 5  
          * (See Second lecture in CAN series - last few slides) */
-        .FilterIdHigh = NODE_ID_SC << 5,      			
+        .FilterIdHigh = 0x0100 << 5,      			
         /* Same as example in lecture 
          * (this gives a range of ID's that will be accepted of between 0x100 and 0x103). 
          * Must also align the bits in the Mask register with those in the receive register. */
