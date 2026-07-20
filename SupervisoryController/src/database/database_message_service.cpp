d4886f9c4464597b630977e6559d32877d9d3126
Nigel Sinclair <sincngraeme@gmail.com>
2026-07-20 14:37:24 -0400

    removed syncstream beacause rpi said no

--- SupervisoryController/src/database/database_message_service.cpp
+++ SupervisoryController/src/database/database_message_service.cpp
@@ -7,7 +7,6 @@
 #include "supervisory/database/database_message_service.hpp"
 #include "supervisory/common/result.hpp"
 #include <iostream>
-#include <syncstream>
 
 namespace project6::supervisory
 {
@@ -73,7 +72,7 @@ cDBMessageService::~cDBMessageService()
         std::cout << ", SQLState: " << e.getSQLState() << ")" << std::endl;
         return ecOperationStatus::DatabaseException;
     }
-    std::osyncstream(std::clog) << "starting worker thread" << std::endl;
+    std::clog << "starting worker thread" << std::endl;
     // Worker thread initialization
     worker_ = std::jthread([this](const std::stop_token stopToken){
         run(stopToken);
