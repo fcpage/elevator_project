633fb8053b0ea9ee396917bf804d186e0ebdba67
fcpage <Colmanfp@gmail.com>
2026-07-24 01:50:48 -0400

    Database schema updates and sql reset/init edit
    Fixed sql init
    modified schema init
    added sql reset - wipes all mysql related data from system, must reinitialize after running.

--- BASH_setup/setup_scripts/sql/user_init.sql
+++ BASH_setup/setup_scripts/sql/user_init.sql
@@ -1,5 +1,5 @@
 USE mysql;
-CREATE USER 'pi'@'%' IDENTIFIED BY 'ese';
-GRANT ALL PRIVILEGES ON *.* TO 'pi'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
+CREATE USER 'pi'@'%' IDENTIFIED BY 'ese';	/*making the rpi username and password*/
+GRANT ALL PRIVILEGES ON *.* TO 'pi'@'%' WITH GRANT OPTION;	/*give it your all!*/
+FLUSH PRIVILEGES;	/*splash*/
 quit;
