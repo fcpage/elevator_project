abeba9f061b436bc2ea6c2d01ecfa53ab922c297
Nigel Sinclair <sincngraeme@gmail.com>
2026-07-24 21:36:57 -0400

    updated request scheduler to include database requests

--- SupervisoryController/src/scheduler/request_scheduler.cpp
+++ SupervisoryController/src/scheduler/request_scheduler.cpp
@@ -38,6 +38,7 @@ void cRequestScheduler::enqueueEvent(const sSupervisoryEvent& event)
             break;
         }
 
+        case ecEventType::DatabaseFloorRequest: [[fallthrough]];
         case ecEventType::HttpFloorRequest:
         {
             request.source = RequestSource::WebInterface;
